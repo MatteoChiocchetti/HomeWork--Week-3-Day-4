7cbb2615dbd2a75f94ee301895ca37e063438eec
Your Name <ch>
2025-03-27 15:29:38 +0100

    adds changes

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-window.addEventListener(DOMContentLoaded, function () {});
+const btn = document.getElementById("btn");
+const cellElement = document.getElementById("cells");
+
+for (let i = 1; i <= 90; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.innerText = i;
+  cellElement.appendChild(cell);
+}
