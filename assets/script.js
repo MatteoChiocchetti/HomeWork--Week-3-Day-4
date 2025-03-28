a7694c256093dad929b164409c0d271ae2d4a170
Chiocchetti Matteo <166328997+MatteoChiocchetti@users.noreply.github.com>
2025-03-28 08:17:38 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ for (let i = 1; i <= 90; i++) {
   cellElement.appendChild(cell);
 }
 const clickedBtn = function () {
-  let randomNum = Math.floor(Math.random() * 89);
+  let randomNum = Math.floor(Math.random() * 90)+1;
   console.log(randomNum);
   const cells = document.querySelectorAll(".cell");
   cells[randomNum].classList.add("selected");
